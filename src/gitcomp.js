e54430b40a78b90cc01007c4ccd800b12a2d7312
Bivas <dasbivas321@gmail.com>
2020-09-09 20:24:45 +0530

    Sorting based algo developed

--- src/gitcomp.js
+++ src/gitcomp.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import axios from 'axios';
 import { Card } from 'antd';
+import useForceUpdate from 'use-force-update';
+import 'antd/dist/antd.css';
+const gridStyle = {
+    width: '25%',
+    textAlign: 'center',
+  };
+  
 
 class GitCompare extends React.Component
 {
@@ -9,6 +16,10 @@ class GitCompare extends React.Component
         super(props)
         this.state={user:[]};
         this.getUserDetails=this.getUserDetails.bind(this);
+        this.sortFollowing=this.sortFollowing.bind(this);
+        this.sortRepo=this.sortRepo.bind(this);
+        this.sortGist=this.sortGist.bind(this);
+        this.sortFollower=this.sortFollower.bind(this);
     }
     getUserDetails()
     {
@@ -30,24 +41,129 @@ class GitCompare extends React.Component
             })
             .catch(error=>
                 {
-                    console.log(error)
+                    console.log(error);
+                    alert("Enter valid UserId");
                 })
+
+    }
+    sortFollower(alpha)
+    {
+        console.log(alpha);
+        var x=this.state.user;
+        for(var i=0;i<x.length;i++)
+        {
+            for(var j=0;j<x.length;j++)
+            {
+                if(x[i].followers>x[j].followers)
+                {
+                    var y=x[i];
+                    x[i]=x[j];
+                    x[j]=y;
+                }
+            }
+        }
+        this.state.user=x;
+        console.log("sorted");
+        console.log(this.state.user)
+        this.forceUpdate();
+    
+
+    }
+    sortFollowing(alpha)
+    {
+        console.log(alpha);
+        var x=this.state.user;
+        for(var i=0;i<x.length;i++)
+        {
+            for(var j=0;j<x.length;j++)
+            {
+                if(x[i].following>x[j].following)
+                {
+                    var y=x[i];
+                    x[i]=x[j];
+                    x[j]=y;
+                }
+            }
+        }
+        this.state.user=x;
+        console.log("sorted");
+        console.log(this.state.user)
+        this.forceUpdate();
+    
+
+    }
+    sortRepo(alpha)
+    {
+        console.log(alpha);
+        var x=this.state.user;
+        for(var i=0;i<x.length;i++)
+        {
+            for(var j=0;j<x.length;j++)
+            {
+                if(x[i].public_repos>x[j].public_repos)
+                {
+                    var y=x[i];
+                    x[i]=x[j];
+                    x[j]=y;
+                }
+            }
+        }
+        this.state.user=x;
+        console.log("sorted");
+        console.log(this.state.user)
+        this.forceUpdate();
+    
+
+    }
+    sortGist(alpha)
+    {
+        console.log(alpha);
+        var x=this.state.user;
+        for(var i=0;i<x.length;i++)
+        {
+            for(var j=0;j<x.length;j++)
+            {
+                if(x[i].public_gists>x[j].public_gists)
+                {
+                    var y=x[i];
+                    x[i]=x[j];
+                    x[j]=y;
+                }
+            }
+        }
+        this.state.user=x;
+        console.log("sorted");
+        console.log(this.state.user)
+        this.forceUpdate();
+    
 
     }
     render()
     {
-        return(<div style={{backgroundColor:"#4b9c3e",height:"100vh"}}>
-            <h1 style={{textAlign:"center",fontSize:"50px",color:"#d1cdcd"}}>GITHUBCOMPARE</h1>
-            <input type="text" id="userid" style={{height:"40px",borderRadius:"20px",width:"300px"}}></input>
+        return(<div style={{backgroundColor:"#25212c"}}>
+            <h1 style={{textAlign:"center",fontSize:"50px",color:"#d1cdcd"}}> Git  -comp</h1>
+            <div style={{display:"flex",alignItems:"center",justifyContent:"center"}}>
+            <input type="text" id="userid" style={{height:"40px",borderRadius:"10px",width:"300px",padding:"0px",fontSize:"30px"}}></input>
             <br></br>
-        <button onClick={this.getUserDetails} style={{height:"40px",borderRadius:"20px",width:"100px",textAlign:"center"}}>Enter UserID </button>
+        <button onClick={this.getUserDetails} style={{height:"40px",borderRadius:"10px",borderWidth:"0px",borderColor:"transparent",margin:"0px",width:"100px",textAlign:"center",marginLeft:"10px"}}>COMPARE</button></div>
+        <br></br>
+        <div style={{display:"flex",alignItems:"center",justifyContent:"center",fontSize:"20px"}}>
+        <button onClick={this.sortFollower}style={{marginLeft:"10px",marginRight:"10px"}}>Arrange by Followers</button>        
+        <button onClick={this.sortFollowing} style={{marginLeft:"10px",marginRight:"10px"}}>Arrange by Following</button>        
+        <button onClick={this.sortRepo} style={{marginLeft:"10px",marginRight:"10px"}}>Arrange by Repo</button>        
+        <button onClick={this.sortGist} style={{marginLeft:"10px",marginRight:"10px"}}>Arrange by Gist</button>        
+        </div>
     <h1>{this.state.user.name}</h1>
     {this.state.user.map((each)=>
-        <div style={{borderColor:"black",borderWidth:"10px"}}><Card title={each.name} extra={<a href={each.html_url}>Check Profile</a>} style={{ width: 300 ,border:"10px",borderColor:"black"}}>
-      <p>Followers:{each.followers}  |  Following:{each.following}</p>
-      <p></p>
-      <p>Card content</p>
-    </Card></div>)}
+    <div className="site-card-border-less-wrapper" style={{width:400,margin:0,marginLeft:"35%"}}>
+    <Card title={each.name}  style={{ width: 500, backgroundColor:"rgb(120 141 160)",fontSize:"23px",textDecoration:"bold"}}>
+    <Card.Grid style={gridStyle,{fontSize:"20px"}}>Followers: {each.followers}</Card.Grid>
+    <Card.Grid style={gridStyle,{fontSize:"20px"}}>Following:{each.following}</Card.Grid>
+    <Card.Grid style={gridStyle,{fontSize:"20px"}}>Repo : {each.public_repos}</Card.Grid>
+    <Card.Grid style={gridStyle,{fontSize:"20px"}}>Gist : {each.public_gists}</Card.Grid>
+    </Card>
+    <br></br>
+  </div>)}
     </div>);
     }
 }
